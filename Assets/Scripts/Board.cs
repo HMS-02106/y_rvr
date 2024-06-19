6fc0f40116a46e4d5780e9b4a46dd01db54f6795
ymstrya <yutorisan19@gmail.com>
2024-06-20 02:58:59 +0900

    中心に持ってくる

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -21,5 +21,10 @@ public class Board : MonoBehaviour
                 var squareSize = newSquare.SpriteSize;
                 newSquare.transform.position = new Vector2(coord.x * squareSize.x, coord.y * squareSize.y);
             });
+        // 中心に持ってくる
+        this.transform.position = new Vector2(
+            this.transform.position.x - square.SpriteSize.x * (size.x - 1) / 2,
+            this.transform.position.y - square.SpriteSize.y * (size.y - 1) / 2
+        );
     }
 }
