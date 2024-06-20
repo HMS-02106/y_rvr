2ebe716f728a9657cff056e0446306993296bb28
ymstrya <yutorisan19@gmail.com>
2024-06-21 01:11:32 +0900

    マウスホバーで選択状態に

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityUtility.Extensions;
 
 /// <summary>
 /// リバーシ盤面のマス
 /// </summary>
-public class Square : MonoBehaviour
+public class Square : MonoBehaviour/*, IMouseInputReceiver*/
 {
     [SerializeField]
     private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private GameObject border;
 
     public Vector2 SpriteSize => spriteRenderer.bounds.size.DisZ();
-    
+
+    void OnMouseOver()
+    {
+        border.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        border.SetActive(false);
+    }
+
     void Start()
     {
         
