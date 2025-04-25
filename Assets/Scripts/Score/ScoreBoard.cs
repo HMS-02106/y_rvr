91b15684f1fd0eef1bd74a20cbdee4272d699905
ymstrya <yutorisan19@gmail.com>
2025-04-25 10:52:32 +0900

    ディレクトリ移動

--- Assets/Scripts/Score/ScoreBoard.cs
+++ Assets/Scripts/Score/ScoreBoard.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 using R3;
 
 public class ScoreBoard : MonoBehaviour
