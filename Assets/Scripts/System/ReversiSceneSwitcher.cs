e2067620f99e0100b0c6142f1ec6c98ba32bc4e0
ymstrya <yutorisan19@gmail.com>
2025-05-21 00:57:35 +0900

    異常な入力でゲームを開始できないようにする

--- Assets/Scripts/System/ReversiSceneSwitcher.cs
+++ Assets/Scripts/System/ReversiSceneSwitcher.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class ReversiSceneSwitcher : MonoBehaviour
 {
@@ -7,19 +8,47 @@ public class ReversiSceneSwitcher : MonoBehaviour
     private TextMeshProUGUI widthTextField;
     [SerializeField]
     private TextMeshProUGUI heightTextField;
+    [SerializeField]
+    private TextMeshProUGUI errorTextField;
+
+    void Start()
+    {
+        errorTextField.text = "";
+    }
+
     public void Switch()
     {
         // なぜか ZERO WIDTH SPACEが含まれてしまうので削除する
         string widthText = widthTextField.text.Replace("\u200B", "").Trim();
         string heightText = heightTextField.text.Replace("\u200B", "").Trim();
 
-        int width = int.Parse(widthText);
-        int height = int.Parse(heightText);
+        if (!int.TryParse(widthText, out int width))
+        {
+            _ = showError("Please enter an integer for width");
+            return;
+        }
+        if (!int.TryParse(heightText, out int height))
+        {
+            _ = showError("Please enter an integer for height");
+            return;
+        }
+        if (width < 4 || height < 4)
+        {
+            _ = showError("Please enter a width and height of at least 4");
+            return;
+        }
 
         // 盤面サイズをPlayerPrefsに保存
         PlayerPrefs.SetInt("ReversiWidth", width);
         PlayerPrefs.SetInt("ReversiHeight", height);
         // シーン遷移
         UnityEngine.SceneManagement.SceneManager.LoadScene("Reversi");
+    }
+
+    private async Task showError(string message)
+    {
+        errorTextField.text = message;
+        await Task.Delay(2000);
+        errorTextField.text = "";
     }
 }
